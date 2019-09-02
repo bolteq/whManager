1de04846e551cbc02d0f69912de781bfa099afd1
Dawid <admin@dailyit.pl>
2019-09-02 18:29:14 +0200

    Dodanie klasy WorkerApi zamiast pisania komunikacji w kodzie strony // Czy muszę robić Injection w obydwoch plikach ?

---
+++ whManagerUI/Classes/WorkerApi.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using whManagerLIB.Models;
+using Newtonsoft.Json;
+
+namespace whManagerUI.Classes
+{
+    public class WorkerApi
+    {
+        private readonly HttpClient _httpClient;
+
+        public WorkerApi(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<IList<Worker>> get()
+        {
+            string requestEndpoint = "worker";
+            HttpResponseMessage httpResponse = await _httpClient.GetAsync(requestEndpoint);
+            string json = await httpResponse.Content.ReadAsStringAsync();
+            return (JsonConvert.DeserializeObject<IList<Worker>>(json));
+        }
+    }
+}
--- whManagerUI/Pages/Index.cshtml.cs
+++ whManagerUI/Pages/Index.cshtml.cs
@@ -5,33 +5,25 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Newtonsoft.Json;
 using whManagerLIB.Models;
-
+using whManagerUI.Classes;
 
 namespace whManagerUI.Pages
 {
     public class IndexModel : PageModel
     {
         public IList<Worker> Workers { get; set; }
-        public readonly HttpClient _httpClient;
+        private WorkerApi workerApi;
+        private readonly HttpClient _httpClient;
 
         public IndexModel(HttpClient httpClient)
         {
             _httpClient = httpClient;
+            workerApi = new WorkerApi(_httpClient);
         }
-
-        //public async void OnGet()
-        //{
-        //    HttpResponseMessage httpResponse = await _httpClient.GetAsync("worker");
-        //    string json = await httpResponse.Content.ReadAsStringAsync();
-        //    IList<Worker> Workers = JsonConvert.DeserializeObject<IList<Worker>>(json);
-        //}
 
         public async Task OnGetAsync()
         {
-            string requestEndpoint = "worker";
-            HttpResponseMessage httpResponse = await _httpClient.GetAsync(requestEndpoint);
-            string json = await httpResponse.Content.ReadAsStringAsync();
-            Workers = JsonConvert.DeserializeObject<IList<Worker>>(json);
+            Workers = await workerApi.get();
         }
 
     }
